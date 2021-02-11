348ac7ec8ab1b05a7e96ff117d7dc6508993eab8
S Tariq <sameentariq@hotmail.co.uk>
2021-02-11 17:55:19 +0000

    Create ValuesVariables.scala

---
+++ source/Section1/src/stariq/basics/ValuesVariables.scala
@@ -0,0 +1,22 @@
+package stariq.basics
+
+object ValuesVariables extends App {
+
+  val x = 42
+  println(x)
+
+  val sString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aInt: Int = x
+  val aShort: Short = 3455
+  val aLong: Long = 2356345234L
+  val aFloat: Float = 2.4f
+  val aDouble: Double = 2.42
+
+  var t: Int = 45
+  t = 34
+}
+
