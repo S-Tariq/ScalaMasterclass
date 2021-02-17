20e021da4426396c6e4b7884522fa60edbfda32e
S Tariq <sameentariq@hotmail.co.uk>
2021-02-17 23:46:29 +0000

    Minor

--- source/Section1/src/stariq/basics/Recursion.scala
+++ source/Section1/src/stariq/basics/Recursion.scala
@@ -2,6 +2,7 @@ package stariq.basics
 
 object Recursion extends App {
 
+  // Works with smaller numbers
   def factorial(n : Int): Int = {
     if(n == 1) n
     else {
@@ -12,8 +13,11 @@ object Recursion extends App {
     }
   }
   println(factorial(10))
-  //println(factorial(5000)) // Stack overflow
+  // Stack overflow - does not work with big numbers.
+  //println(factorial(5000))
 
+  // Works with bigger numbers
+  // The inside function can run on its own without the outside function.
   def anotherFactorial(n: Int): BigInt = {
     def factorialHelper(x: Int, accumulator: BigInt): BigInt = {
       if(x == 1) accumulator
@@ -21,10 +25,11 @@ object Recursion extends App {
     }
     factorialHelper(n , 1)
   }
-  // This returns zero because of overflow of Int representation. Change Int to BigInt.
+  // This will return zero because of overflow of Int representation. Change Int to BigInt.
   println(anotherFactorial(5000))
 
   /*
+  Step by step:
     anotherFactorial(10) = factorialHelper(10, 1)
     = factorialHelper(9, 10 * 1) The accumulator becomes 10 as it is being passed in as a parameter.
     = factorialHelper(8, 9 * 10) // 9 * 10 * 1
@@ -36,4 +41,8 @@ object Recursion extends App {
     returns the accumulator which is already factorial of n
     as it is 1 * 2 * 3 * 4 * ... * 10 (moved the 1 to the front)
    */
+
+  // The reason why the first method factorial fails but the second method anotherFactorial does not fail
+  // for big numbers is because in the second method, the recursive method is written as the last expression
+  // whereas in the first one, it is not on its own as last expression because it is multiplying by n.
 }
