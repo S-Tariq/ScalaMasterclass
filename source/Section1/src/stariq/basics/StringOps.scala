f909fa929d450e3448f7ba009499e8a893a583c4
S Tariq <sameentariq@hotmail.co.uk>
2021-03-11 23:12:33 +0000

    Update StringOps.scala

--- source/Section1/src/stariq/basics/StringOps.scala
+++ source/Section1/src/stariq/basics/StringOps.scala
@@ -14,4 +14,27 @@ object StringOps extends App {
 
   val aNumberStr: String = "45"
   val aNumber = aNumberStr.toInt
+  println('a' +: aNumberStr :+ 'z')
+
+  println(str.reverse)
+  println(str.take(2))
+
+  // S-interpolators
+  val name = "David"
+  val age = 12
+  val greeting = s"Hello, my name is $name and I am $age years old."
+  val anotherGreeting = s"Hello, my name is $name and I will be turning ${age + 1} years old."
+  println(anotherGreeting)
+
+  // F-interpolators
+  val speed = 1.2f
+  val myth = f"$name can eat $speed%2.2f burgers per minute."
+  println(myth)
+
+  // Raw-interpolators
+  val literal = raw"This is a \n newline."
+  println(literal)
+  val escaped = "This is a \n newline."
+  println(raw"$escaped")
+
 }
