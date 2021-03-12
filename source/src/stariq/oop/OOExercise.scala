26453e42fea71b054523d332354b8333aefb57e1
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2021-03-12 17:41:17 +0000

    Minor

---
+++ source/src/stariq/oop/OOExercise.scala
@@ -0,0 +1,25 @@
+package stariq.oop
+
+/*
+Novel and Writer
+
+Writer: first name, surname, year
+- method fullname
+
+Novel: name, year of release, author
+- method authorAge
+- method isWrittenBy(author)
+- method copy (new year of release) - new instance of Novel
+ */
+
+/*
+Counter class:
+- receives an int value
+- method current count
+- method to increment/decrement - new Counter
+-overload inc/dec to receive an amount
+ */
+
+object OOExercise extends App {
+
+}
