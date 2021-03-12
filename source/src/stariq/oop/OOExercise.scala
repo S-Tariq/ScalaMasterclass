3db0fdbdcf412a896cd78140f56fcdaf460c757c
S Tariq <sameentariq@hotmail.co.uk>
2021-03-12 23:23:31 +0000

    Update OOExercise.scala

--- source/src/stariq/oop/OOExercise.scala
+++ source/src/stariq/oop/OOExercise.scala
@@ -1,5 +1,7 @@
 package stariq.oop
 
+import java.time.Year
+
 /*
 Novel and Writer
 
@@ -21,5 +23,62 @@ Counter class:
  */
 
 object OOExercise extends App {
+
+  val author = new Writer("Sam", "Ben", 1999)
+  val imposter = new Writer("Sam", "Ben", 1999)
+  val novel = new Novel("Great expectations", 2021, imposter)
+
+  println(novel.authorAge())
+  // Imposter has same data as author but it returns false as it checks the object reference.
+  println(novel.isWrittenBy(author))
+
+  val counter = new Counter
+  counter.increment.print
+  counter.increment.increment.increment.print
+  counter.increment(10).print
+}
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName(): String = {
+    firstName + " " + surname
+  }
+}
+
+class Novel(name: String, year: Int, author: Writer) {
+  def authorAge(): Int = {
+    year - author.year
+  }
+
+  def isWrittenBy(author: Writer): Boolean = {
+    author == this.author
+  }
+
+  def copy(newYear: Int): Novel = {
+    new Novel(name, newYear, author)
+  }
+}
+
+class Counter(val count: Int = 0) {
+  // All methods are immutable.
+  def increment = new Counter(count + 1)
+  def decrement = new Counter(count - 1)
+
+  // If the methods are used for logging where each event needs to be tracked,
+  // you can write a recursive method rather than the following method which skips steps
+  // and directly increments/decrements it by n.
+//  def increment(n: Int) = new Counter(count + n)
+//  def decrement(n: Int) = new Counter(count - n)
+
+  def increment(n: Int): Counter = {
+    if(n <= 0) this
+    else increment.increment(n - 1)
+  }
+
+  def decrement(n: Int): Counter = {
+    if(n <= 0) this
+    else decrement.decrement(n - 1)
+  }
+
+  def print = println(count)
 
 }
